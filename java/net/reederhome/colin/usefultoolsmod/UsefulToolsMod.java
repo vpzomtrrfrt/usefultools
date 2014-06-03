43290d1f7bab558bff5b75f58080329efc2cd47c
Colin Reeder <vpzomtrrfrt@gmail.com>
2014-06-03 13:07:19 -0600

    Added glowstone crops

--- java/net/reederhome/colin/usefultoolsmod/UsefulToolsMod.java
+++ java/net/reederhome/colin/usefultoolsmod/UsefulToolsMod.java
@@ -5,11 +5,13 @@ import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.reederhome.colin.usefultoolsmod.client.UsefulToolsClient;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.registry.EntityRegistry;
 import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.relauncher.Side;
@@ -19,15 +21,17 @@ import cpw.mods.fml.relauncher.SideOnly;
 public class UsefulToolsMod {
 
 	public static final String MODID = "usefultools";
-	public static final String VERSION = "0.1";
+	public static final String VERSION = "0.2pre";
 	
 	static Block autoClicker = new BlockAutoClicker();
 	static Block obsidiPlate = new BlockObsidiPlate();
+	static Block glowstoneCrops = new BlockGlowstoneCrops();
 	
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent ev) {
 		GameRegistry.registerBlock(autoClicker, "autoClicker");
 		GameRegistry.registerBlock(obsidiPlate, "obsidiPlate");
+		GameRegistry.registerBlock(glowstoneCrops, "glowstoneCrops");
 		MinecraftForge.EVENT_BUS.register(this);
 		
 		GameRegistry.addRecipe(new ItemStack(autoClicker), "www", "wrw", "rpr", 'w', Blocks.planks, 'r', Blocks.cobblestone, 'p', Items.redstone);
@@ -45,5 +49,16 @@ public class UsefulToolsMod {
 	@SideOnly(Side.CLIENT)
 	public void clientInit(FMLInitializationEvent ev) {
 		UsefulToolsClient.registerRenderers();
+	}
+	
+	@SubscribeEvent
+	public void onInteract(PlayerInteractEvent ev) {
+		ItemStack stack = ev.entityLiving.getHeldItem();
+		if(stack!=null) {
+			if(stack.getItem().equals(Items.glowstone_dust)&&ev.entity.worldObj.getBlock(ev.x, ev.y, ev.z).equals(Blocks.farmland)) {
+				ev.entity.worldObj.setBlock(ev.x, ev.y+1, ev.z, glowstoneCrops);
+				stack.stackSize--;
+			}
+		}
 	}
 }
