5f3bfe45e66c173c2dfb3814c493ec9eb698743b
Colin Reeder <vpzomtrrfrt@gmail.com>
2015-10-26 09:33:52 -0600

    Left-clicks too

--- java/net/reederhome/colin/usefultoolsmod/BlockClickSpreader.java
+++ java/net/reederhome/colin/usefultoolsmod/BlockClickSpreader.java
@@ -21,10 +21,14 @@ public class BlockClickSpreader extends Block {
 	public IIcon getIcon(int side, int meta) {
 		return side==0?UsefulToolsMod.autoClicker.getIcon(0,0):(side==1?Blocks.furnace.getIcon(1,1):blockIcon);
 	}
+	
+	public int getCoord(int b) {
+		return b+new Random().nextInt(5)-2;
+	}
 
 	public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int p_149727_6_, float p_149727_7_, float p_149727_8_, float p_149727_9_) {
-		int nx = x+new Random().nextInt(5)-2;
-		int nz = z+new Random().nextInt(5)-2;
+		int nx = getCoord(x);
+		int nz = getCoord(z);
 		int ny = y-1;
 		while(y>=0) {
 			Block b = world.getBlock(nx, ny, nz);
@@ -35,5 +39,18 @@ public class BlockClickSpreader extends Block {
 			y--;
 		}
 		return true;
+	}
+	public void onBlockClicked(World world, int x, int y, int z, EntityPlayer player) {
+		int nx = getCoord(x);
+		int nz = getCoord(z);
+		int ny = y-1;
+		while(y>=0) {
+			Block b = world.getBlock(nx, ny, nz);
+			if(b!=null && b!=Blocks.air) {
+				b.onBlockClicked(world, nx, ny, nz, player);
+				break;
+			}
+			y--;
+		}
 	}
 }
